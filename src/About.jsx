1881f40819175861817a1b1a6258f7ae0106fa7e
neducod <nedunony16@gmail.com>
2026-04-01 17:57:00 +0100

    About section done

--- src/About.jsx
+++ src/About.jsx
@@ -6,7 +6,7 @@ import AboutImage from './assets/hamburger-french-fries-cheeseburger-chicken-san
 function AboutSection (){
     return(
         <>
-        <div className='flex sm:hidden lg:hidden flex-col gap-6 p-6 justify-center items-center    relative w-ful mb-50'>
+        <div className='flex sm:hidden lg:hidden flex-col gap-6 p-6 justify-center items-center    relative w-ful mb-60'>
             {/* ?\FOR SMALLER SCREEN */}
             <div className='flex flex-col gap-4 font-bold  absolute top-30 left-0 w-full z-10 text-white text-center pt-5'>
                 <h2 className='text-3xl'>About Tasty <span className='text-red-600 font-bold italic underline decoration-wavy decoration-orange-400'>Foods</span></h2>
@@ -14,17 +14,23 @@ function AboutSection (){
                     <p className='text-black'>Explicabo tempore magni a aliquam necessitatibus molestias odio facere. Ipsa, unde animi adipisci, incidunt nam officiis vitae recusandae dolore nisi necessitatibus exercitationem.</p>
                     <p className='text-black'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
                 </div>
-                <div className='flex flex-col justify-center pl-8 text-black'>
-                    <div className='flex flex-row items-center'>
-                        <div>1</div>
+                <div className='flex gap-2 flex-col justify-center pl-8 text-black'>
+                    <div className='flex  gap-4 flex-row items-center'>
+                        <div className='bg-red-500 text-white w-8 h-8 rounded-t-full rounded-b-full rounded-l-full rounded-r-full text-center flex items-center justify-center'>
+                            <p>1</p>
+                        </div>
                         <p>Delicious and healthy foods</p>
                     </div>
-                    <div className='flex flex-row items-center'>
-                        <div>2</div>
+                    <div className='flex gap-4 flex-row items-center'>
+                        <div className='bg-red-500 text-white w-8 h-8 rounded-t-full rounded-b-full rounded-l-full rounded-r-full text-center flex items-center justify-center'> 
+                            <p>2</p>
+                        </div>
                         <p>Best price and Offers</p>
                     </div>
-                    <div className='flex flex-row items-center'>
-                        <div>3</div>
+                    <div className='flex gap-4 flex-row items-center'>
+                        <div className='bg-red-500 text-white w-8 h-8 rounded-t-full rounded-b-full rounded-l-full rounded-r-full text-center flex items-center justify-center'>
+                            <p>3</p>
+                        </div>
                         <p>Made with fresh ingredients</p>
                     </div>
                 </div>
